53c4ab7cf052adc46219a5c20db2726483b34c52
Wonjong Park <wonjong2@gmail.com>
2022-06-04 12:06:18 -0700

    Changes eventhandlers for the landing page

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,33 +1,33 @@
 // Event handlers for the 'Login(Order Now)' button
 const loginFormHandler = async (event) => {
-    // event.preventDefault();
+    event.preventDefault();
 
-    // // Collect values from the login form
-    // const user_email = document.querySelector('#username-login').value.trim();
-    // const password = document.querySelector('#password-login').value.trim();
+    // Collect values from the login form
+    const user_email = document.querySelector('#login-email').value.trim();
+    const password = document.querySelector('#login-pwd').value.trim();
 
-    // if (user_email && password) {
-    //     // Send a POST request to the API endpoint
-    //     const response = await fetch('/api/users/login', {
-    //         method: 'POST',
-    //         body: JSON.stringify({ user_email, password }),
-    //         headers: { 'Content-Type': 'application/json' },
-    //     });
+    if (user_email && password) {
+        // Send a POST request to the API endpoint
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ user_email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
 
-    //     if (response.ok) {
-    //         // If successful, redirect the browser to the main page
-    //         document.location.replace('/api/menu');
-    //     } else {
-    //         alert(response.statusText);
-    //     }
-    // }
+        if (response.ok) {
+            // If successful, redirect the browser to the main page
+            document.location.replace('/api/menu');
+        } else {
+            alert(response.statusText);
+        }
+    }
 }
 
 // Event handler for the 'Order as Guest' button
-const guestOrderHandler = async (event) => {
-    // event.preventDefault();
-    // document.location.replace('/api/menu');
-}
+// const guestOrderHandler = async (event) => {
+// event.preventDefault();
+// document.location.replace('/api/menu');
+// }
 
 // Event handler for the 'Signup' button
 const signupFormHandler = async (event) => {
@@ -35,40 +35,46 @@ const signupFormHandler = async (event) => {
     // Need to add the code to compare passwords entered by the user
     ///////////////////////////////////////////////////////////////////
 
-    // event.preventDefault();
+    event.preventDefault();
 
-    // const user_email = document.querySelector('#username-signup').value.trim();
-    // const password = document.querySelector('#password-signup').value.trim();
+    const user_email = document.querySelector('#signup-email').value.trim();
+    const password = document.querySelector('#signup-pwd').value.trim();
+    const confirmPassword = document.querySelector('#signup-cnfPwd').value.trim();
 
-    // if (user_email && password) {
-    //     const response = await fetch('/api/users', {
-    //         method: 'POST',
-    //         body: JSON.stringify({ user_email, password }),
-    //         headers: { 'Content-Type': 'application/json' },
-    //     });
-    //     console.log(response);
-    //     if (response.ok) {
-    //         document.location.replace('/api/menu');
-    //     } else {
-    //         alert(response.statusText);
-    //     }
-    // }
+    if (password === confirmPassword) {
+        if (user_email && password) {
+            const response = await fetch('/api/users', {
+                method: 'POST',
+                body: JSON.stringify({ user_email, password }),
+                headers: { 'Content-Type': 'application/json' },
+            });
+
+            if (response.ok) {
+                document.location.replace('/api/menu');
+            } else {
+                alert(response.statusText);
+            }
+        }
+    }
+    else {
+        alert("Password and Confirm Password do not match. Please enter them again!");
+    }
 }
 
 // Event handler for the 'View Status' button
 const viewStatusHandler = async (event) => {
-    // console.log(event);
-    // const id = event.target.dataset.postId ? event.target.dataset.postId : event.target.parentElement.dataset.postId;
+    event.preventDefault();
+    const orderNumber = document.querySelector('#order-num').value.trim();
 
-    // if (id) {
-    //     document.location.assign(`/${id}`);
-    // }
+    if (orderNumber) {
+        document.location.assign(`/${orderNumber}`);
+    }
 }
 
 // 'login (Order Now)' button
-// document
-//     .querySelector('.login-form')
-//     .addEventListener('submit', loginFormHandler);
+document
+    .querySelector('#login-btn')
+    .addEventListener('click', loginFormHandler);
 
 // 'Order as Guest' button
 // document
@@ -76,11 +82,11 @@ const viewStatusHandler = async (event) => {
 //     .addEventListener('submit', guestOrderHandler);
 
 // 'Signup' button
-// document
-//     .querySelector('.signup-form')
-//     .addEventListener('submit', signupFormHandler);
+document
+    .querySelector('#signup-btn')
+    .addEventListener('click', signupFormHandler);
 
 // 'View Status' button
-// document
-//     .querySelector('.viewStatus')
-//     .addEventListener('submit', viewStatusHandler);
+document
+    .querySelector('#check-order-btn')
+    .addEventListener('click', viewStatusHandler);
