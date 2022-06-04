24f93cd8f21d968675af0ce355fcd5b3324b4787
Vlad Berka Desktop <vlad.m.berka@gmail.com>
2022-06-04 13:44:12 -0700

    working on order history handlebar

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { User, Menu, OrderHistory, Categories } = require("../models");
 const withAuth = require("../utils/auth");
+const {orderHistory_to_Array} = require("../utils/helpers");
 
 // GET for the homepage
 router.get("/", async (req, res) => {
@@ -13,7 +14,7 @@ router.get("/", async (req, res) => {
 });
 
 // GET route for order status (will need withAuth), ID is order number
-router.get("/:id", async (req, res) => {
+router.get("/orderhistory/:id", async (req, res) => {
   try {
     const findOrder = await OrderHistory.findByPk(req.params.id, {
       include: [
@@ -24,15 +25,35 @@ router.get("/:id", async (req, res) => {
     });
 
     // console.log(findOrder);
-    console.log("Findorder is:", findOrder);
     const getOrderStatus = findOrder.get({ plain: true });
-    console.log("getOrderStatus is:", getOrderStatus);
+    console.log(getOrderStatus);
+
+    var item_list_array = orderHistory_to_Array(getOrderStatus.item_list);
+
+    try {
+      const menu_list_all = await Menu.findAll({
+        where: {
+          id: item_list_array
+        }
+      });
+
+      console.log("Menu list all is: ", menu_list_all);
+      var menu_list = menu_list_all.map((item) => item.get({ plain: true }));
+      // const menu_list = menu_list_all.get({ plain: true });
+      console.log("Menu list is: ", menu_list);
+
+
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+
     //to check with Insomnia
     // res.status(200).json(getOrderStatus);
     res.render("order-summary", {
       getOrderStatus,
+      menu_list,
     });
-    
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
