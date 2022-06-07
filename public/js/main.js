0f20a82603e089096d91ec6c6d2f27e791ccdd16
Wonjong Park <wonjong2@gmail.com>
2022-06-06 19:17:13 -0700

    Changes event handler for the menu page

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-// Variable to have the latest cart menu
+// Variable to have the latest cart menu, Array of menu objects
 let cartMenu = [];
 
 // Who is the current user?
@@ -7,28 +7,29 @@ const getUserInfo = () => document.querySelector().textContent;
 /**
  * Function for getting the cart data from the localStorage
  * @param {string} user : Current user's name or email
- * @returns {array} : Array of menu objects (id, name, price)
+ * @returns {array} : Array of menu objects (id, name, price, qty)
  */
 const getCartFromStorage = (user) => JSON.parse(localStorage.getItem(user));
 
 /**
  * Function for saving data to the localStorage
  * @param {string} user : Current user's name or email
- * @param {array} menulist : Array of menu objects (id, name, price)
+ * @param {array} menulist : Array of menu objects (id, name, price, qty)
  * @returns {void} Nothing
  */
-const saveCartToStorage = (user, menuList) => localStorage.setItem(user, JSON.stringify(menuList));
+const saveCartToStorage = (user, menuList) =>
+    localStorage.setItem(user, JSON.stringify(menuList));
 
 /**
  * Function for adding a menu to the cart and the localStorage
- * @param {object} menu : Selected menu by user (id, name, price)
+ * @param {object} menu : Selected menu by user (id, name, price, qty)
  * @returns {void} Nothing
  */
 const addMenuToCart = (menu) => {
     cartMenu.push(menu);
     const user = getUserInfo();
     saveCartToStorage(user, cartMenu);
-}
+};
 
 /**
  * Function for deleting a menu from the cart and the localStorage
@@ -39,7 +40,7 @@ const deleteMenuFromCart = (index) => {
     cartMenu.splice(index, 1);
     const user = getUserInfo();
     saveCartToStorage(user, cartMenu);
-}
+};
 
 /**
  * Function for the adding a menu to the cart and the localStorage
@@ -47,61 +48,74 @@ const deleteMenuFromCart = (index) => {
  * @returns {void} Nothing
  */
 const renderCart = () => {
+    const totalPrice = 0;
     const cartEl = document.querySelector();
 
-    for (let i = 0; i < cartMenu.length; i++) {
-        var menu = document.createElement('li');
-        menu.textContent = cartMenu[i];
+    while (cartEl.firstChild) {
+        cartEl.removeChild(cartEl.firstChild);
     }
-}
+
+    for (let i = 0; i < cartMenu.length; i++) {
+        var menu = document.createElement("li");
+        menu.innerHTML = `${cartMenu[i].name}($${cartMenu[i].price}) <i class="bi bi-trash"></i>`;
+        cartEl.appendChild(menu);
+        totalPrice += cartMenu[i].price * cartMenu[i].qty;
+    }
+};
 
 // Event handlers for clicking a menu from menu lists
 const selectMenuHandler = async (event) => {
     // Update the Cart
     // And then save menu lists in the Cart to the localstorage
-}
+    let id = event.target;
+    let name = event.target;
+    let price = event.target;
+
+    const menu = {
+        id,
+        name,
+        price,
+        qty: 1,
+    };
+
+    addMenuToCart(menu);
+    renderCart();
+};
 
 // Event handler for deletion from the cart menus
 const deleteMenuHandler = async (event) => {
     // DELETE ("api/order/cart/:id") - delete an item in the cart Trash can next to item in cart
-
     // event.preventDefault();
-
     // const id = document.querySelector('.card').dataset.postId;
-
     // if (id) {
     //     const response = await fetch(`/api/order/cart/${id}`, {
     //         method: 'DELETE',
     //     });
-
     //     if (response.ok) {
     //         document.location.assign('/api/menu');
     //     } else {
     //         alert(response.statusText);
     //     }
     // }
-}
+};
 
 // Event handler for the 'Submit Order' button
 const submitOrderHandler = async (event) => {
     // POST ("api/order/order-summary") - create the order and render the order summary page "Submit Order" button at the bottom of cart
-
     // event.preventDefault();
-
     //     // Send a PUT request to the API endpoint
     //     const response = await fetch('/api/order/order-summary', {
     //         method: 'POST',
-    //         
+    //         body: JSON.stringify({ cartMenu }),
+    //         headers: { 'Content-Type': 'application/json' },
     //     });
-
     //     if (response.ok) {
     //         document.location.replace('??');
     //     } else {
     //         alert(response.statusText);
     //     }
     // }
-
-}
+};
 
 // Click a menu item
 // document
