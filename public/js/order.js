206ae237747cd38498a099ab800e54e65291c53d
Vlad Berka Desktop <vlad.m.berka@gmail.com>
2022-06-08 20:31:14 -0700

    Fixing countdown timer

--- public/js/order.js
+++ public/js/order.js
@@ -18,37 +18,43 @@ const cancelOrderHandler = async (event) => {
 //Countdown Timer in the
 function Timer() {
   var imported_time = document.querySelector(".countdown_timer").textContent;
+
   if (imported_time == "Order ready for pickup!") {
     var secondsLeft = 0;
   }
   else {
-    var secondsLeft = parseInt(imported_time);
+    var secondsLeft = Math.floor(parseInt(imported_time));
   }
   var seconds_remaining = secondsLeft;
   var seconds_remaining_string = "";
-  console.log(imported_time);
+  var minute_count=0;
+  console.log("Imported seconds are: ",imported_time);
 
   // Sets interval in variable
   var timerInterval = setInterval(function () {
     // Each time the interval triggers, we subtract an interval amount
     secondsLeft--;
+    minute_count=0;
     seconds_remaining = secondsLeft;
     // This updates the timer count at the top of the webpage
-    while (seconds_remaining > 600) {
-      seconds_remaining -= 600;
+    while (seconds_remaining >= 60) {
+      seconds_remaining -= 60;
+      minute_count++;
     }
 
-    if (seconds_remaining > 100) {
+    if (seconds_remaining >= 10 || seconds_remaining == 0) {
       seconds_remaining_string = seconds_remaining;
     } else {
-      seconds_remaining_string = `0${Math.floor(seconds_remaining / 10)}`;
+      seconds_remaining_string = `0${Math.floor(seconds_remaining)}`;
     }
 
     if (imported_time != "Order ready for pickup!") {
       if (document.querySelector(".countdown_timer") != null) {
         document.querySelector(".countdown_timer").textContent =
-          "0" + Math.floor(secondsLeft / 60) + ":" + seconds_remaining_string;
+          "0" + minute_count + ":" + seconds_remaining_string;
       }
+      // Displays countdown in seconds
+      // document.querySelector(".countdown_timer").textContent = Math.floor(secondsLeft);
     };
     // If statement filters if we've run out of time
     if (secondsLeft <= 0) {
@@ -62,7 +68,7 @@ function Timer() {
       // Clears the interval to stop counting  time
       clearInterval(timerInterval);
     }
-  }, 100); // NOTE: DECI-SECOND NOTATION, EVERY 1/10th OF A SECOND WE COUNT DOWN
+  }, 1000); // NOTE: MILLI-SECOND NOTATION, EVERY 1/10th OF A SECOND WE COUNT DOWN
 }
 
 //Starts running the countdown timer by default
